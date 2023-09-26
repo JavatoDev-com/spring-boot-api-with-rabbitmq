325ee0d738c08905287ae4020706c4e19bce689f
Lakshitha Warnakulasuriya <lakshitha.warnakulasuriya@ifsworld.com>
2023-09-26 12:33:11 +0530

    Added test class

---
+++ spring-boot-api-with-rabbitmq/src/test/java/com/javatodev/app/SpringBootApiWithRabbitmqApplicationTests.java
@@ -0,0 +1,12 @@
+package com.javatodev.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootApiWithRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}

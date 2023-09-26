d348e802fb2beee3ccd53c7d7801f30b5cabb413
Lakshitha Warnakulasuriya <lakshitha.warnakulasuriya@ifsworld.com>
2023-09-26 12:15:32 +0530

    Initial Commit

---
+++ spring-boot-apt-with-rabbitmq/src/main/java/com/javatodev/app/controller/MessageController.java
@@ -0,0 +1,44 @@
+package com.javatodev.app.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.javatodev.app.dto.User;
+import com.javatodev.app.publisher.RabbitMQJsonProducer;
+import com.javatodev.app.publisher.RabbitMQProducer;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+
+    private RabbitMQProducer rabbitMQProducer;
+
+    private RabbitMQJsonProducer rabbitMQJsonProducer;
+
+    public MessageController(RabbitMQProducer rabbitMQProducer, RabbitMQJsonProducer rabbitMQJsonProducer) {
+        this.rabbitMQProducer = rabbitMQProducer;
+        this.rabbitMQJsonProducer = rabbitMQJsonProducer;
+    }
+
+    /**
+     * API for publish message for RabbitMQ queue
+     * @param message
+     * @return ResponseEntity
+     */
+    @GetMapping("/publish")
+    public ResponseEntity<String> sendMessage(@RequestParam("message") String message){
+        rabbitMQProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to RabbitMQ.");
+    }
+
+    /**
+     * API for publish message for RabbitMQ json queue
+     * @param user
+     * @return ResponseEntity
+     */
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user){
+        rabbitMQJsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("Json message sent to RabbitMQ.");
+    }
+}
